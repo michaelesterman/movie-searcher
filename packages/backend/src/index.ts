9302059ae4dbb2c9ed5be9073991965ce0afab74
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-03-04 13:10:13 +0200

    Add hello world API

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello from backend");
+import "dotenv/config";
+
+import express from "express";
+import cors from "cors";
+
+const main = async () => {
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.get("/", (req, res) => {
+    res.send("This is the API. Next versions will add authorization.");
+  });
+
+  app.listen(port, () => {
+    console.log(`🚀 Development API started at http://localhost:${port}`);
+  });
+};
+
+main();
