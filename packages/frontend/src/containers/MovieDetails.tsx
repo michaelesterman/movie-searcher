8a566d10e9bfe377cdbe81f0f87f031087a5261f
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-03-05 00:02:25 +0200

    Add movie details stub

---
+++ packages/frontend/src/containers/MovieDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const MovieDetails = () => {
+  return (
+    <div>
+      <div>MovieDetails</div>
+    </div>
+  );
+};
+
+export default MovieDetails;
